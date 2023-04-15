584fb8c5cc2b5f96bde14921451381f97f5358a5
BillySwCodeNinjas <129233042+WalmartBagGuy@users.noreply.github.com>
2023-04-15 14:33:29 -0500

    DroppingBombs2 Final
    End of Dropping Bombs Part 2

--- BS - Dropping Bombs 2/Assets/Scripts/RocketAnimation.cs
+++ BS - Dropping Bombs 2/Assets/Scripts/RocketAnimation.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class RocketAnimation : MonoBehaviour
 {
+    [Header("Animator")]
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+
+        animator.SetFloat("HAaxis", horizontal);
     }
 }
