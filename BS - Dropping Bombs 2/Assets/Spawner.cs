f4039b99715ddf2d7db567abef006d0fd0cc7cc0
BillySwCodeNinjas <130806226+MinnieMia@users.noreply.github.com>
2023-05-06 13:56:46 -0500

    Dropping Bombs 3
    Finished Dropping Bombs Part 3

--- BS - Dropping Bombs 2/Assets/Spawner.cs
+++ BS - Dropping Bombs 2/Assets/Spawner.cs
@@ -18,6 +18,10 @@ public class Spawner : MonoBehaviour
     {
         ResetDelay();
         StartCoroutine(EnemyGenerator());
+
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        objectWidth = bombPrefab.GetComponent<MeshRenderer>().bounds.size.x / 2;
+        objectHeight = bombPrefab.GetComponent<MeshRenderer>().bounds.size.y / 2;
     }
 
     // Update is called once per frame
@@ -26,9 +30,12 @@ public class Spawner : MonoBehaviour
         
     }
 
-    IEnumerator EnemtGenerator(){
+    IEnumerator EnemyGenerator(){
         yield return new WaitForSeconds(delay);
         if(active){
+            float randomX = Random.Range(screenBounds.x - objectWidth, screenBounds.x * -1 + objectWidth);
+            float spawnY = (screenBounds.y + objectHeight) + 5;
+            
             Instantiate(bombPrefab, new Vector3(randomX, spawnY, 0), bombPrefab.transform.rotation);
             ResetDelay();
         }
